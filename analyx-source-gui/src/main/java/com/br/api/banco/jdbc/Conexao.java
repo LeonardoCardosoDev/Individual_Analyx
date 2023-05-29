ca8a7012fd60e2af78c627bc6b687d30112bceec
LeonardoCardosoDev <leonardo.cardoso@sptech.school>
2023-05-28 21:55:41 -0300

    Update

--- analyx-source-gui/src/main/java/com/br/api/banco/jdbc/Conexao.java
+++ analyx-source-gui/src/main/java/com/br/api/banco/jdbc/Conexao.java
@@ -16,7 +16,7 @@ public class Conexao {
 
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
 
-        dataSource.setUrl("jdbc:mysql://0.0.0.0:3306/bd_analyx?autoReconnect=true&useSSL=false");;
+        dataSource.setUrl("jdbc:mysql://ContainerAnalyx/bd_analyx");;
 
         dataSource.setUsername("root");
 
@@ -27,7 +27,6 @@ public class Conexao {
 //        dataSource.setUsername("");
 //
 //        dataSource.setPassword("");
-
         this.conexaoDoBanco = new JdbcTemplate(dataSource);
     }
 
